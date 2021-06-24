4094e6a0e2997ee7ae355909a209aef85161f085
OGane99 <80991754+OGane99@users.noreply.github.com>
2021-06-24 12:35:18 -0400

    Update SolarEnergyBalances.m

--- SolarEnergyBalances.m
+++ SolarEnergyBalances.m
@@ -1,6 +1,4 @@
 function F = SolarEnergyBalances(x,y,z,wLevel,sqArea)
-    %EVERYTHING AS HEAT FLUX CURRENTLY
-    
     solar_flux = y;   %fnc takes solar flux as input
     Ta = z;        %fnc takes ambient air temp as input
     Tsky = 0.0552*Ta^(1.5);  %sky temperature
@@ -24,23 +22,18 @@ function F = SolarEnergyBalances(x,y,z,wLevel,sqArea)
     wallHeight = 0.04;      %height of solar still walls
     wallThick = 0.01;       %thickness of solar still walls
     coverAngle = 20;        %angle the glass makes with respect to horizon
-    A = sqArea;                  %water and basin surface area
+    A = sqArea;             %water and basin surface area
     Ag = 2*(sqrt(A)*(sqrt(A)*0.5)/cosd(coverAngle));     %glass surface area
     Ains = sqrt(A) + 4*wallHeight*sqrt(A);
     
-  
-    %Hc_ga = 10;             %convection between glass and air
-    Hc_ga = 20;
+    Hc_ga = 20;             %convection between glass and air
     Hc_ia = 2;
     
     Kw = 0.6;               %conduction coef for water
     Lw = wLevel;            %height of water
-    %Ki = 0.06;
     Ki = 0.04;              %conduction coef of insulation
     Li = 0.02;              %height of insulation
-    %Li = 0.01;
 
-    
     Pw = exp(25.317-5144/(273.15+x(2)));    %partial pressure of vapor at water
     Pg = exp(25.317-5144/(273.15+x(1)));    %partial pressure of vapor at glass
 
@@ -54,9 +47,6 @@ function F = SolarEnergyBalances(x,y,z,wLevel,sqArea)
     F(2)=(A*solar_flux*w_absorb + A*(Kw/Lw)*(x(3)-x(2)) - A*Hc_gw*(x(2)-x(1)) - A*He_gw*(x(2)-x(1)) - A*SBConst*w_emis*(((x(2)+273.15)^4)-((x(1)+273.15)^4)));
     
     %basin energy balance
-    %F(3)=(A*solar_flux*b_absorb - Ains*(Ki/Li)*(((x(3)+x(2)+x(1))/3)-Ta) - A*(Kw/Lw)*(x(3)-x(2)));
-    %F(3)=(A*solar_flux*b_absorb - Ains*(Ki/Li)*(((x(3)+x(2)+x(1))/3)-x(4)) - A*(Kw/Lw)*(x(3)-x(2)));
-    %F(3)=(A*solar_flux*b_absorb - Ains*(Ki/Li)*(x(3)-x(4)) - A*(Kw/Lw)*(x(3)-x(2)));
     F(3)=(A*solar_flux*b_absorb - Ains*(Ki/Li)*(x(3)-((Ta+x(3))/2)) - A*(Kw/Lw)*(x(3)-x(2)));
 
     
